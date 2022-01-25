6fbaf35dd2db7afe5ffd85b353a2fa25b10405d7
kamilagruhlke <kgruhlke@gmail.com>
2022-01-25 13:57:40 +0100

    Created new ASP.NET CORE MVC App

---
+++ Finest/Controllers/FinestController.cs
@@ -0,0 +1,79 @@
+﻿using Finest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Finest.Controllers
+{
+    public class FinestController : Controller
+    {
+        private static IList<FinestModel> finest = new List<FinestModel>();
+        public ActionResult Index()
+        {
+            return View(finest);
+        }
+
+        // GET: FinestController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View(new FinestModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FinestModel finestModel, IFormCollection collection)
+        {
+            finestModel.Id = finest.Count + 1;
+            finest.Add(finestModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: FinestController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FinestController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FinestController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FinestController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Finest/Models/FinestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Finest.Models
+{
+    public class FinestModel
+    {
+        [DisplayName("Numer")]
+        public int Id { get; set; }
+        [DisplayName("Nazwa")]
+        public string Name { get; set; }
+        [DisplayName("Opis")]
+        public string Description { get; set; }
+        [DisplayName("Data realizacji")]
+        public DateTime Date { get; set; }
+        public bool Completed { get; set; }
+    }
+}
